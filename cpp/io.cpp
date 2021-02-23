df4cefab0b7186270474df4c15e4ece7c7356379
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2021-02-23 17:11:17 +0200

    More C to C++

--- cpp/io.cpp
+++ cpp/io.cpp
@@ -15,46 +15,46 @@ void write_field(Field& field, int const iter, ParallelData const parallel)
 {
     char filename[64];
 
-    /* The actual write routine takes only the actual data
-     * (without ghost layers) so we need array for that. */
-    double *full_data;
-    double *tmp_data;          // array for MPI sends and receives
-
     auto height = field.nx * parallel.size;
     auto width = field.ny;
 
-    auto data = field.temperature.data();
-
-    tmp_data = new double [field.nx * field.ny];
+    // array for MPI sends and receives
+    auto tmp_vec = std::vector<double> (field.nx * field.ny); 
 
     if (parallel.rank == 0) {
         /* Copy the inner data */
-        full_data = new double [height * width];
-        for (int i = 0; i < field.nx; i++)
-	  memcpy(&full_data[i * width], &data[(i + 1) * (width + 2) + 1],
-                   field.ny * sizeof(double));
+        auto full_data = std::vector<double> (height * width);
+        for (int i = 0; i < field.nx; i++) {
+            auto start = (i + 1) * (width + 2) + 1;
+            auto end = (i + 1) * (width + 2) + 1 + width;
+            auto dest = i * width;
+            std::copy(field.temperature.begin() + start, field.temperature.begin() + end,
+                      full_data.begin() + dest);
+        }
+          
         /* Receive data from other ranks */
         for (int p = 1; p < parallel.size; p++) {
-            MPI_Recv(tmp_data, field.nx * field.ny,
+            MPI_Recv(tmp_vec.data(), field.nx * field.ny,
                      MPI_DOUBLE, p, 22, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             /* Copy data to full array */
-            memcpy(&full_data[p * field.nx * width], tmp_data,
-                   field.nx * field.ny * sizeof(double));
+            std::copy(tmp_vec.begin(), tmp_vec.begin() + field.nx * field.ny,
+                      full_data.begin() + p * field.nx * width);
         }
         /* Write out the data to a png file */
         std::sprintf(filename, "%s_%04d.png", "heat", iter);
-        save_png(full_data, height, width, filename, 'c');
-        delete[] full_data;
+        save_png(full_data.data(), height, width, filename, 'c');
     } else {
         /* Send data */
-        for (int i = 0; i < field.nx; i++)
-	  memcpy(&tmp_data[i * width], &data[(i + 1) * (width + 2) + 1],
-                   field.ny * sizeof(double));
-        MPI_Send(tmp_data, field.nx * field.ny,
+        for (int i = 0; i < field.nx; i++) {
+            auto start = (i + 1) * (width + 2) + 1;
+            auto end = (i + 1) * (width + 2) + 1 + width;
+            auto dest = i * width;
+            std::copy(field.temperature.begin() + start, field.temperature.begin() + end,
+                       tmp_vec.begin() + dest);
+        }
+        MPI_Send(tmp_vec.data(), field.nx * field.ny,
                  MPI_DOUBLE, 0, 22, MPI_COMM_WORLD);
     }
-
-    delete[] tmp_data;
 
 }
 
@@ -65,11 +65,7 @@ void read_field(Field& field, std::string filename,
                 ParallelData const parallel)
 {
     FILE *fp;
-    int nx, ny, ind;
-    double *full_data;
-    double *inner_data;
-
-    int nx_local, ny_local, count;
+    int nx, ny, ind, count;
 
     fp = fopen(filename.c_str(), "r");
     /* Read the header */
@@ -82,12 +78,11 @@ void read_field(Field& field, std::string filename,
 
     field.setup(nx, ny, parallel);
 
-    inner_data = new double[field.nx * field.ny];
+    /* Full array */
+    auto full = std::vector<double> (nx * ny);
+    auto full_data = full.data();
 
     if (parallel.rank == 0) {
-        /* Full array */
-        full_data = new double [nx * ny];
-
         /* Read the actual data */
         for (int i = 0; i < nx; i++) {
             for (int j = 0; j < ny; j++) {
@@ -95,35 +90,36 @@ void read_field(Field& field, std::string filename,
                 count = fscanf(fp, "%lf", &full_data[ind]);
             }
         }
-    } else {
-        /* Dummy array for full data. Some MPI implementations
-         * require that this array is actually allocated... */
-        full_data = new double[1];
     }
 
-    nx_local = field.nx;
-    ny_local = field.ny;
+    auto nx_local = field.nx;
+    auto ny_local = field.ny;
 
-    MPI_Scatter(full_data, nx_local * ny, MPI_DOUBLE, inner_data,
+    // Inner region (no boundaries)
+    auto inner = std::vector<double> (field.nx * field.ny);
+    // auto innea_data = inner.data();
+    MPI_Scatter(full.data(), nx_local * ny, MPI_DOUBLE, inner.data(),
                 nx_local * ny, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     /* Copy to the array containing also boundaries */
-    auto data = field.temperature.data();
-    for (int i = 0; i < nx_local; i++)
-      memcpy(&data[(i + 1) * (ny_local + 2) + 1], &inner_data[i * ny_local],
-               ny * sizeof(double));
+    for (int i = 0; i < nx_local; i++) {
+      auto start = i * ny_local;    // beginning of row
+      auto end = i * ny_local + ny; // end of row
+      auto dest = (i + 1) * (ny_local + 2) + 1; // beginnig of inner region
+      std::copy(inner.begin() + start, inner.begin() + end,
+                field.temperature.begin() + dest);
+    }
 
     /* Set the boundary values */
     for (int i = 1; i < nx_local + 1; i++) {
-        data[i * (ny_local + 2)] = data[i * (ny_local + 2) + 1];
-        data[i * (ny_local + 2) + ny + 1] = data[i * (ny_local + 2) + ny];
+        field.temperature[i * (ny_local + 2)] = field.temperature[i * (ny_local + 2) + 1];
+        field.temperature[i * (ny_local + 2) + ny + 1] = field.temperature[i * (ny_local + 2) + ny];
     }
     for (int j = 0; j < ny + 2; j++) {
-        data[j] = data[ny_local + j];
-        data[(nx_local + 1) * (ny_local + 2) + j] = data[nx_local * (ny_local + 2) + j];
+        field.temperature[j] = field.temperature[ny_local + j];
+        field.temperature[(nx_local + 1) * (ny_local + 2) + j] = 
+          field.temperature[nx_local * (ny_local + 2) + j];
     }
 
-    delete[] full_data;
-    delete[] inner_data; 
     fclose(fp);
 }
