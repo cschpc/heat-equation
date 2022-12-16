cc75fce65906d14884bd5718c40d301484492151
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2022-12-16 10:22:14 +0200

    First try for OpenACC + HIP
    Compiles and runs but gives incorrect result

---
+++ 3d/openacc-hip/fortran/core.F90
@@ -0,0 +1,139 @@
+! Main solver routines for heat equation solver
+module core
+  use heat
+  use openacc
+  use, intrinsic :: iso_c_binding
+
+  interface
+
+    subroutine evolve_hip(curr, prev, a, dt, nx, ny, nz, dx, dy, dz) bind(C, name='evolve')
+      USE, intrinsic :: iso_c_binding
+#ifdef _CRAYFTN
+      type(c_ptr), intent(inout) :: curr, prev
+#else
+      type(c_devptr), intent(inout) :: curr, prev
+#endif
+      real(c_double), intent(in) :: a, dt, dx, dy, dz
+      integer(c_int), intent(in) :: nx, ny, nz
+    end subroutine evolve_hip
+
+  end interface
+
+contains
+
+  ! Exchange the boundary data between MPI tasks
+  subroutine exchange(field0, parallel)
+    use mpi
+
+    implicit none
+
+    type(field), target, intent(inout) :: field0
+    type(parallel_data), intent(in) :: parallel
+
+    real(dp), pointer, contiguous, dimension(:,:,:) :: data
+
+    integer :: buf_size
+
+    integer :: ierr
+
+    data => field0%data
+
+    buf_size = (field0%nx + 2) * (field0%ny + 2)
+
+#ifdef GPU_MPI
+!$acc host_data use_device(data)
+#else
+!$acc update host(data)
+#endif
+    ! Send to left, receive from right
+    call mpi_sendrecv(data(:, :, 1), buf_size, MPI_DOUBLE_PRECISION, &
+         & parallel%nleft, 11, &
+         & data(:, :, field0%nz + 1), buf_size, MPI_DOUBLE_PRECISION, &
+         & parallel%nright, 11, &
+         & MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+
+    ! Send to right, receive from left
+    call mpi_sendrecv(data(:, :, field0%nz), buf_size, MPI_DOUBLE_PRECISION, &
+         & parallel%nright, 12, &
+         & data(:, :, 0), buf_size, MPI_DOUBLE_PRECISION,&
+         & parallel%nleft, 12, &
+         & MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+#ifdef GPU_MPI
+!$acc end host_data
+#else
+!$acc update device(data)
+#endif
+  end subroutine exchange
+
+  ! Compute one time step of temperature evolution
+  ! Arguments:
+  !   curr (type(field)): current temperature values
+  !   prev (type(field)): values from previous time step
+  !   a (real(dp)): update equation constant
+  !   dt (real(dp)): time step value
+  subroutine evolve(curr, prev, a, dt)
+
+    implicit none
+
+    type(field), target, intent(inout) :: curr, prev
+    real(kind=dp), intent(in) :: a, dt
+
+    real(dp), pointer, contiguous, dimension(:,:,:) :: currdata, prevdata
+    integer :: i, j, k, nx, ny, nz
+    real(kind=dp) :: dx, dy, dz
+
+    nx = curr%nx
+    ny = curr%ny
+    nz = curr%nz
+
+    dx = curr%dx
+    dy = curr%dy
+    dz = curr%dz
+
+    currdata => curr%data
+    prevdata => prev%data
+
+    call evolve_hip(acc_deviceptr(currdata), acc_deviceptr(prevdata), a, dt, nx, ny, nz, dx, dy, dz)
+  end subroutine evolve
+
+  ! Start a data region and copy temperature fields to the device
+  !   curr (type(field)): current temperature values
+  !   prev (type(field)): values from previous time step
+  subroutine enter_data(curr, prev)
+    implicit none
+    type(field), target, intent(in) :: curr, prev
+    real(kind=dp), pointer, contiguous :: currdata(:,:,:), prevdata(:,:,:)
+
+    currdata => curr%data
+    prevdata => prev%data
+
+    !$acc enter data copyin(currdata, prevdata)
+  end subroutine enter_data
+
+  ! End a data region and copy temperature fields back to the host
+  !   curr (type(field)): current temperature values
+  !   prev (type(field)): values from previous time step
+  subroutine exit_data(curr, prev)
+    implicit none
+    type(field), target :: curr, prev
+    real(kind=dp), pointer, contiguous :: currdata(:,:,:), prevdata(:,:,:)
+
+    currdata => curr%data
+    prevdata => prev%data
+
+    !$acc exit data copyout(currdata, prevdata)
+  end subroutine exit_data
+
+  ! Copy a temperature field from the device to the host
+  !   temperature (type(field)): temperature field
+  subroutine update_host(temperature)
+    implicit none
+    type(field), target :: temperature
+    real(kind=dp), pointer, contiguous :: tempdata(:,:,:)
+
+    tempdata => temperature%data
+
+    !$acc update host(tempdata)
+  end subroutine update_host
+
+end module core
---
+++ 3d/openacc-hip/fortran/heat_mod.F90
@@ -0,0 +1,126 @@
+! Field metadata for heat equation solver
+module heat
+  use iso_fortran_env, only : REAL64
+
+  implicit none
+
+  integer, parameter :: dp = REAL64
+  real(dp), parameter :: DX = 0.01, DY = 0.01, DZ = 0.01  ! Fixed grid spacing
+
+  type :: field
+     integer :: nx          ! local dimension of the field
+     integer :: ny
+     integer :: nz
+     integer :: nx_full     ! global dimension of the field
+     integer :: ny_full
+     integer :: nz_full
+     real(dp) :: dx
+     real(dp) :: dy
+     real(dp) :: dz
+     real(dp), dimension(:,:,:), allocatable :: data
+  end type field
+
+  type :: parallel_data
+     integer :: size
+     integer :: rank
+     integer :: nleft, nright  ! Ranks of neighbouring MPI tasks
+     integer :: dev_count
+  end type parallel_data
+
+contains
+  ! Initialize the field type metadata
+  ! Arguments:
+  !   field0 (type(field)): input field
+  !   nx, ny, dx, dy: field dimensions and spatial step size
+  subroutine set_field_dimensions(field0, nx, ny, nz, parallel)
+    implicit none
+
+    type(field), intent(out) :: field0
+    integer, intent(in) :: nx, ny, nz
+    type(parallel_data), intent(in) :: parallel
+
+    integer :: nx_local, ny_local, nz_local
+
+    nx_local = nx
+    ny_local = ny
+    nz_local = nz / parallel%size
+
+    field0%dx = DX
+    field0%dy = DY
+    field0%dz = DZ
+    field0%nx = nx_local
+    field0%ny = ny_local
+    field0%nz = nz_local
+    field0%nx_full = nx
+    field0%ny_full = ny
+    field0%nz_full = nz
+
+  end subroutine set_field_dimensions
+
+  subroutine parallel_setup(parallel, nx, ny, nz)
+    use mpi
+#ifdef _OPENACC
+    use openacc
+#endif
+
+    implicit none
+
+    type(parallel_data), intent(out) :: parallel
+    integer, intent(in), optional :: nx, ny, nz
+
+    integer :: nz_local
+    integer :: ierr
+
+#ifdef _OPENACC
+    integer :: node_rank, node_procs, my_device
+    integer :: intranodecomm
+#endif
+
+    call mpi_comm_size(MPI_COMM_WORLD, parallel%size, ierr)
+
+    if (present(nz)) then
+       nz_local = nz / parallel%size
+       if (nz_local * parallel%size /= nz) then
+          write(*,*) 'Cannot divide grid evenly to processors'
+          call mpi_abort(MPI_COMM_WORLD, -2, ierr)
+       end if
+    end if
+
+    call mpi_comm_rank(MPI_COMM_WORLD, parallel%rank, ierr)
+
+    parallel%nleft = parallel%rank - 1
+    parallel%nright = parallel%rank + 1
+
+    if (parallel%nleft < 0) then
+       parallel%nleft = MPI_PROC_NULL
+    end if
+    if (parallel%nright > parallel%size - 1) then
+       parallel%nright = MPI_PROC_NULL
+    end if
+
+    parallel%dev_count = 0
+#ifdef _OPENACC
+    call mpi_comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0,  MPI_INFO_NULL, intranodecomm, ierr);
+
+    call mpi_comm_rank(intranodecomm, node_rank, ierr);
+    call mpi_comm_size(intranodecomm, node_procs, ierr);
+
+    call mpi_comm_free(intranodecomm, ierr)
+
+    parallel%dev_count = acc_get_num_devices(acc_get_device_type())
+    my_device = mod(node_rank, parallel%dev_count)
+
+    if (node_procs > parallel%dev_count) then
+       if (parallel%rank == 0) then
+          write(*, '(AI4AI4)') 'Oversubscriging GPUs: MPI tasks per node: ', &
+                &               node_procs, ' GPUs per node: ', parallel%dev_count
+       end if
+    end if
+
+    call acc_set_device_num(my_device, acc_get_device_type())
+#endif
+
+
+  end subroutine parallel_setup
+
+end module heat
