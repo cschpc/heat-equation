767d959da92d7344ee78449ba288ec2777da0fce
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-04-24 11:40:45 +0300

    Add CMake version of cpp heat equation

---
+++ ctest-gtest/src/parallel.cpp
@@ -0,0 +1,18 @@
+#include "heat.hpp"
+#include <mpi.h>
+
+ParallelData::ParallelData() {
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    nup = rank - 1;
+    ndown = rank + 1;
+
+    if (nup < 0) {
+        nup = MPI_PROC_NULL;
+    }
+    if (ndown > size - 1) {
+        ndown = MPI_PROC_NULL;
+    }
+}
