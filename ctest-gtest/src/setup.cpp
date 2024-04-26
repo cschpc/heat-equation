5f45a6f4d7b76275af9209429470a552613ba0a6
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-04-26 17:02:09 +0300

    Add TODO comments

--- ctest-gtest/src/setup.cpp
+++ ctest-gtest/src/setup.cpp
@@ -4,11 +4,20 @@
 #include "parallel.hpp"
 #include <iostream>
 
+// TODO:
+// - use heat::read_field or heat::generate_field to get data
+// - partition the data to the processes/ranks in a similar fashion as is done
+// in io.cpp:read_field, i.e. use MPI_scatter with the full_data
+// - add constructor to field that takes (local) num_rows and num_cols and local
+// data
+// - in the constructor generate the ghost data as in io.cpp:read_field
+// - while doing this, add tests for each section (before if possible)
 Field initialize(const heat::Input &input, const ParallelData &parallel) {
     Field field = {};
     if (input.read_file) {
-        if (0 == parallel.rank)
+        if (0 == parallel.rank) {
             std::cout << "Reading input from " + input.fname << std::endl;
+        }
         read_field(field, input.fname, parallel);
     } else {
         field.setup(input.rows, input.cols, parallel);
