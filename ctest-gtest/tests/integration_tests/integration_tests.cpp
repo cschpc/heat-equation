5a5739aff2e22ab1b6d625d7135dab3fd283c67b
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-05-02 13:38:26 +0300

    Refactor integration tests to separate files

--- ctest-gtest/tests/integration_tests/integration_tests.cpp
+++ ctest-gtest/tests/integration_tests/integration_tests.cpp
@@ -3,7 +3,6 @@
 #include <gtest/gtest.h>
 #include <mpi.h>
 
-#include "io.hpp"
 #include "parallel.hpp"
 #include "pngwriter.h"
 
@@ -41,6 +40,7 @@ bool loadPng(const char *fname, PngData &png) {
     return false;
 }
 
+// TODO: make MPI test and test with this input and with default
 TEST(integration_test, image_matches_reference) {
     int argc = 1;
     char **argv = NULL;
@@ -69,38 +69,4 @@ TEST(integration_test, image_matches_reference) {
     }
 
     MPI_Finalize();
-}
-
-TEST(integration_test, default_input_ok) {
-    const heat::Input default_input = {};
-    const heat::Input input = heat::read_input("", 0);
-    ASSERT_EQ(input, default_input) << "input is different from default_input";
-}
-
-TEST(integration_test, input_from_file_ok) {
-    const heat::Input input = heat::read_input("testdata/input.json", 0);
-    const heat::Input default_input = {};
-    ASSERT_NE(input, default_input) << "input is equal to default_input";
-}
-
-TEST(integration_test, input_from_nonexistent_path_throws_exception) {
-    EXPECT_THROW(
-        {
-            try {
-                const heat::Input input =
-                    heat::read_input("batman vs superman", 0);
-            } catch (const std::runtime_error &e) {
-                EXPECT_STREQ("Non-existent path: \"batman vs superman\"",
-                             e.what());
-                throw;
-            }
-        },
-        std::runtime_error);
-}
-
-TEST(integration_test, read_field_data_from_file) {
-    auto [num_rows, num_cols, data] = heat::read_field("testdata/bottle.dat");
-    ASSERT_EQ(data.size(), 40000);
-    ASSERT_EQ(num_rows * num_cols, data.size());
-    ASSERT_GT(data.size(), 0) << "data vector should contain some data";
 }
