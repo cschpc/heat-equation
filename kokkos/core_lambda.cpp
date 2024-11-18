f6869e66e171adcff76d64363731bd2677c79dca
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2024-11-18 17:02:08 -0500

    Evolve with functor

--- kokkos/core_lambda.cpp
+++ kokkos/core_lambda.cpp
@@ -10,24 +10,22 @@ void exchange(Field& field, const ParallelData parallel)
 {
 
     // Send to up, receive from down
-    // auto sbuf = field.temperature(1, 0).data();
-    auto sbuf = field.temperature.data();
-    //auto rbuf  = field.temperature(field.nx + 1, 0).data();
-    auto rbuf  = field.temperature.data();
+    auto sbuf = Kokkos::subview (field.temperature, 1, Kokkos::ALL).data();
+    auto rbuf = Kokkos::subview (field.temperature, field.nx + 1, Kokkos::ALL).data();
     MPI_Sendrecv(sbuf, field.ny + 2, MPI_DOUBLE,
                  parallel.nup, 11,
                  rbuf, field.ny + 2, MPI_DOUBLE, 
                  parallel.ndown, 11, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     // Send to down, receive from up
-    // sbuf = field.temperature(field.nx, 0).data();
-    sbuf = field.temperature.data();
+    sbuf = Kokkos::subview (field.temperature, 
+                                    field.nx,
+                                    Kokkos::ALL).data();
     rbuf = field.temperature.data();
     MPI_Sendrecv(sbuf, field.ny + 2, MPI_DOUBLE, 
                  parallel.ndown, 12,
                  rbuf, field.ny + 2, MPI_DOUBLE,
                  parallel.nup, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
 }
 
 // Update the temperature values using five-point stencil */
@@ -40,10 +38,8 @@ void evolve(Field& curr, Field& prev, const double a, const double dt)
   using MDPolicyType = Kokkos::MDRangePolicy<Kokkos::Rank<2> >;
   MDPolicyType mdpolicy({1, 1}, {curr.nx + 1, curr.ny + 1});
 
-  Kokkos::View<double**, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged> >
-       curr_temp (curr.temperature.data(), curr.nx + 2, curr.ny + 2);
-  Kokkos::View<const double**, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged> >
-       prev_temp (prev.temperature.data(), prev.nx + 2, prev.ny + 2);
+  auto curr_temp = curr.temperature;
+  auto prev_temp = prev.temperature;
   
   Kokkos::parallel_for("evolve", mdpolicy, 
      KOKKOS_LAMBDA(const int i, const int j) {
