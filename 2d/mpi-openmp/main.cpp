cfb7fef1eeaf1118d3616e8ae844068e6a606da1
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-06-19 11:09:52 +0300

    Change omp single to omp master & barrier and change MPI therad support to funneled: Scalasca doesn't support other threaded MPI

--- 2d/mpi-openmp/main.cpp
+++ 2d/mpi-openmp/main.cpp
@@ -4,80 +4,84 @@
 
 /* Heat equation solver in 2D. */
 
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <mpi.h>
 #ifdef _OPENMP
 #include <omp.h>
 #endif
 
 #include "heat.h"
 
-int main(int argc, char **argv)
-{
-    double a = 0.5;             //!< Diffusion constant
-    field current, previous;    //!< Current and previous temperature fields
+int main(int argc, char **argv) {
+    double a = 0.5;          //!< Diffusion constant
+    field current, previous; //!< Current and previous temperature fields
 
-    double dt;                  //!< Time step
-    int nsteps;                 //!< Number of time steps
+    double dt;  //!< Time step
+    int nsteps; //!< Number of time steps
 
-    int image_interval = 1500;    //!< Image output interval
+    int image_interval = 1500; //!< Image output interval
 
     parallel_data parallelization; //!< Parallelization info
 
-    double dx2, dy2;            //!< Delta x and y squared
+    double dx2, dy2; //!< Delta x and y squared
 
-    double average_temp;        //!< Average temperature
+    double average_temp; //!< Average temperature
 
-    double start_clock, stop_clock;  //!< Time stamps
+    double start_clock, stop_clock; //!< Time stamps
 
-    int provided;             // thread-support level
+    int provided;                              // thread-support level
+    const int requested = MPI_THREAD_FUNNELED; // thread-support level
 
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
-    if (provided < MPI_THREAD_MULTIPLE) {
-        printf("MPI_THREAD_MULTIPLE thread support level required\n");
+    MPI_Init_thread(&argc, &argv, requested, &provided);
+    if (provided < requested) {
+        printf("MPI thread support level not high enough\n");
         MPI_Abort(MPI_COMM_WORLD, 5);
     }
 
-    #pragma omp parallel 
+#pragma omp parallel
     {
-    initialize(argc, argv, &current, &previous, &nsteps, &parallelization);
+        initialize(argc, argv, &current, &previous, &nsteps, &parallelization);
 
-    /* Output the initial field */
-    #pragma omp single
-    {
-    write_field(&current, 0, &parallelization);
+        /* Output the initial field */
+#pragma omp master
+        {
+            write_field(&current, 0, &parallelization);
 
-    average_temp = average(&current);
-    if (parallelization.rank == 0) {
-        printf("Average temperature at start: %f\n", average_temp);
-    }    
+            average_temp = average(&current);
+            if (parallelization.rank == 0) {
+                printf("Average temperature at start: %f\n", average_temp);
+            }
 
-    /* Largest stable time step */
-    dx2 = current.dx * current.dx;
-    dy2 = current.dy * current.dy;
-    dt = dx2 * dy2 / (2.0 * a * (dx2 + dy2));
-    } // end omp simple
+            /* Largest stable time step */
+            dx2 = current.dx * current.dx;
+            dy2 = current.dy * current.dy;
+            dt = dx2 * dy2 / (2.0 * a * (dx2 + dy2));
+        } // end omp simple
+#pragma omp barrier
 
-    /* Get the start time stamp */
-    start_clock = MPI_Wtime();
+        /* Get the start time stamp */
+        start_clock = MPI_Wtime();
 
-    /* Time evolve */
-    for (int iter = 1; iter <= nsteps; iter++) {
-        #pragma omp single
-        exchange(&previous, &parallelization);
-        evolve(&current, &previous, a, dt);
-        if (iter % image_interval == 0) {
-            #pragma omp single
-            write_field(&current, iter, &parallelization);
+        /* Time evolve */
+        for (int iter = 1; iter <= nsteps; iter++) {
+#pragma omp master
+            exchange(&previous, &parallelization);
+#pragma omp barrier
+            evolve(&current, &previous, a, dt);
+            if (iter % image_interval == 0) {
+#pragma omp master
+                write_field(&current, iter, &parallelization);
+#pragma omp barrier
+            }
+            /* Swap current field so that it will be used
+                as previous for next iteration step */
+#pragma omp master
+            swap_fields(&current, &previous);
+#pragma omp barrier
         }
-        /* Swap current field so that it will be used
-            as previous for next iteration step */
-        #pragma omp single
-        swap_fields(&current, &previous);
-    }
 
     } // end omp parallel
     stop_clock = MPI_Wtime();
--- 2d/mpi-openmp/setup.cpp
+++ 2d/mpi-openmp/setup.cpp
@@ -4,21 +4,19 @@
 
 /* Setup routines for heat equation solver */
 
+#include <assert.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <mpi.h>
 
 #include "heat.h"
 
-
-#define NSTEPS 500  // Default number of iteration steps
+#define NSTEPS 500 // Default number of iteration steps
 
 /* Initialize the heat equation solver */
-void initialize(int argc, char *argv[], field *current,
-                field *previous, int *nsteps, parallel_data *parallel)
-{
+void initialize(int argc, char *argv[], field *current, field *previous,
+                int *nsteps, parallel_data *parallel) {
     /*
      * Following combinations of command line arguments are possible:
      * No arguments:    use default field dimensions and number of time steps
@@ -27,11 +25,10 @@ void initialize(int argc, char *argv[], field *current,
      * Three arguments: field dimensions (rows,cols) and number of time steps
      */
 
-
-    int rows = 2000;             //!< Field dimensions with default values
+    int rows = 2000; //!< Field dimensions with default values
     int cols = 2000;
 
-    char input_file[64];        //!< Name of the optional input file
+    char input_file[64]; //!< Name of the optional input file
 
     int read_file = 0;
 
@@ -67,46 +64,49 @@ void initialize(int argc, char *argv[], field *current,
     }
 
     if (read_file) {
-        #pragma omp single
+#pragma omp master
         read_field(current, previous, input_file, parallel);
+#pragma omp barrier
     } else {
-        #pragma omp single
+#pragma omp master
         {
-        parallel_setup(parallel, rows, cols);
-        set_field_dimensions(current, rows, cols, parallel);
-        set_field_dimensions(previous, rows, cols, parallel);
+            parallel_setup(parallel, rows, cols);
+            set_field_dimensions(current, rows, cols, parallel);
+            set_field_dimensions(previous, rows, cols, parallel);
         }
+#pragma omp barrier
         generate_field(current, parallel);
-        #pragma omp single
+#pragma omp master
         {
-        allocate_field(previous);
-        copy_field(current, previous);
+            allocate_field(previous);
+            copy_field(current, previous);
         }
+#pragma omp barrier
     }
 }
 
 /* Generate initial temperature field.  Pattern is disc with a radius
  * of nx_full / 6 in the center of the grid.
  * Boundary conditions are (different) constant temperatures outside the grid */
-void generate_field(field *temperature, parallel_data *parallel)
-{
+void generate_field(field *temperature, parallel_data *parallel) {
     double radius;
 
     /* Allocate the temperature array, note that
      * we have to allocate also the ghost layers */
-    #pragma omp single
-    temperature->data = new double [(temperature->nx + 2) * (temperature->ny + 2)];
+#pragma omp master
+    temperature->data =
+        new double[(temperature->nx + 2) * (temperature->ny + 2)];
+#pragma omp barrier
 
- 
     /* Radius of the source disc */
     radius = temperature->nx_full / 6.0;
-    #pragma omp for 
+#pragma omp for
     for (int i = 0; i < temperature->nx + 2; i++) {
         for (int j = 0; j < temperature->ny + 2; j++) {
-	    int ind = i * (temperature->ny + 2) + j;
+            int ind = i * (temperature->ny + 2) + j;
             /* Distance of point i, j from the origin */
             int dx = i + parallel->rank * temperature->nx -
-                 temperature->nx_full / 2 + 1;
+                     temperature->nx_full / 2 + 1;
             int dy = j - temperature->ny / 2 + 1;
             if (dx * dx + dy * dy < radius * radius) {
                 temperature->data[ind] = 5.0;
@@ -116,23 +116,25 @@ void generate_field(field *temperature, parallel_data *parallel)
         }
     }
 
-    /* Boundary conditions */
-    #pragma omp for
+/* Boundary conditions */
+#pragma omp for
     for (int i = 0; i < temperature->nx + 2; i++) {
         temperature->data[i * (temperature->ny + 2)] = 20.0;
-        temperature->data[i * (temperature->ny + 2) + temperature->ny + 1] = 70.0;
+        temperature->data[i * (temperature->ny + 2) + temperature->ny + 1] =
+            70.0;
     }
 
     if (parallel->rank == 0) {
-        #pragma omp for
+#pragma omp for
         for (int j = 0; j < temperature->ny + 2; j++) {
             temperature->data[j] = 85.0;
         }
-    } 
+    }
     if (parallel->rank == parallel->size - 1) {
-        #pragma omp for
+#pragma omp for
         for (int j = 0; j < temperature->ny + 2; j++) {
-            temperature->data[(temperature->nx + 1) * (temperature->ny + 2) + j] = 5.0;
+            temperature
+                ->data[(temperature->nx + 1) * (temperature->ny + 2) + j] = 5.0;
         }
     }
 }
@@ -140,8 +142,7 @@ void generate_field(field *temperature, parallel_data *parallel)
 /* Set dimensions of the field. Note that the nx is the size of the first
  * dimension and ny the second. */
 void set_field_dimensions(field *temperature, int nx, int ny,
-                          parallel_data *parallel)
-{
+                          parallel_data *parallel) {
     int nx_local;
 
     nx_local = nx / parallel->size;
@@ -154,8 +155,7 @@ void set_field_dimensions(field *temperature, int nx, int ny,
     temperature->ny_full = ny;
 }
 
-void parallel_setup(parallel_data *parallel, int nx, int ny)
-{
+void parallel_setup(parallel_data *parallel, int nx, int ny) {
     MPI_Comm_size(MPI_COMM_WORLD, &parallel->size);
     MPI_Comm_rank(MPI_COMM_WORLD, &parallel->rank);
 
@@ -172,8 +172,7 @@ void parallel_setup(parallel_data *parallel, int nx, int ny)
     }
 }
 
-void parallel_set_dimensions(parallel_data *parallel, int nx, int ny)
-{
+void parallel_set_dimensions(parallel_data *parallel, int nx, int ny) {
     int nx_local;
 
     nx_local = nx / parallel->size;
@@ -181,14 +180,10 @@ void parallel_set_dimensions(parallel_data *parallel, int nx, int ny)
         printf("Cannot divide grid evenly to processors\n");
         MPI_Abort(MPI_COMM_WORLD, -2);
     }
-
 }
-
 
 /* Deallocate the 2D arrays of temperature fields */
-void finalize(field *temperature1, field *temperature2)
-{
+void finalize(field *temperature1, field *temperature2) {
     delete[] temperature1->data;
-    delete[] temperature2->data; 
+    delete[] temperature2->data;
 }
-
